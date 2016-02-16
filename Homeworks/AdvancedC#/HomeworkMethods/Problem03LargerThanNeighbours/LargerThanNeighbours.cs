2a7e90226ec886f111963fcda65c0d601f6ac57c
VladoVIdenov <videnov852@gmail.com>
2016-02-16 10:41:18 +0200

    HW Methods add

---
+++ Homeworks/AdvancedC#/HomeworkMethods/Problem03LargerThanNeighbours/LargerThanNeighbours.cs
@@ -0,0 +1,61 @@
+﻿namespace Problem03LargerThanNeighbours
+{
+    using System;
+    using System.Security.AccessControl;
+
+    public class LargerThanNeighbours
+    {
+        public static void Main()
+        {
+            int[] arr = { 3, 2, 20, 4, 5, 10, 7, 8, 9, 10, 5 };
+            
+            bool isGreater = false;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                isGreater = CheckIsGreater(arr, i);
+                Console.WriteLine(isGreater);
+            }
+        }
+
+        private static bool CheckIsGreater(int[] arr, int i)
+        {
+            bool isGreater = false;
+            if (i == 0)
+            {
+                if (arr[i] > arr[i + 1])
+                {
+                    isGreater = true;
+                }
+                else
+                {
+                    isGreater = false;
+                }
+            }
+            else if (i == arr.Length - 1)
+            {
+                if (arr[i] > arr[i - 1])
+                {
+                    isGreater = true;
+                }
+                else
+                {
+                    isGreater = false;
+                }
+            }
+            else
+            {
+                if ((arr[i] > arr[i + 1]) && (arr[i] > arr[i - 1]))
+                {
+                    isGreater = true;
+                }
+                else
+                {
+                    isGreater = false;
+                }
+            }
+
+            return isGreater;
+        }
+    }
+}
---
+++ Homeworks/AdvancedC#/HomeworkMethods/Problem07GenericArraySort/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Problem07GenericArraySort
+{
+    using System;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            // Previous Homework Selection Sort used /Problem02
+            int[] myArray = { 1, 6, 15, 99, 6, 18, 2, 3 };
+            string[] stringArr = { "zaz", "cba", "baa", "azis", "ungabunga" };
+            DateTime[] datesArr = { new DateTime(2002, 3, 1), new DateTime(2012, 12, 24), new DateTime(2016, 02, 16), };
+
+            SelectionSort(myArray);
+            SelectionSort(stringArr);
+            SelectionSort(datesArr);
+            Console.WriteLine(string.Join(", ", myArray));
+            Console.WriteLine(string.Join(", ", stringArr));
+            Console.WriteLine(string.Join(", ", datesArr));
+        }
+
+        public static void SelectionSort<T>(T[] myArray) where T : IComparable
+        {
+            for (int i = 0; i < myArray.Length - 1; i++)
+            {
+                int indexOfNewMin = i;
+                for (int j = i + 1; j < myArray.Length; j++)
+                {
+                    if (myArray[j].CompareTo(myArray[indexOfNewMin]) < 0)
+                    {
+                        // Store the index of new minimum value
+                        indexOfNewMin = j;
+                    }
+                }
+
+                if (indexOfNewMin != i)
+                {
+                    T bufferNumber;
+                    bufferNumber = myArray[indexOfNewMin];
+                    myArray[indexOfNewMin] = myArray[i];
+                    myArray[i] = bufferNumber;
+                }
+            }
+        }
+    }
+}
