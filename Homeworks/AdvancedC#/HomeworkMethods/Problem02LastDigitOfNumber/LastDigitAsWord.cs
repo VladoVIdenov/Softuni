677d44bb144bfbf7bd1ea45c6140117d3a405700
VladoVIdenov <videnov852@gmail.com>
2016-02-16 10:57:19 +0200

    HW Methods Add

---
+++ Homeworks/AdvancedC#/HomeworkMethods/Problem02LastDigitOfNumber/LastDigitAsWord.cs
@@ -0,0 +1,21 @@
+﻿namespace Problem02LastDigitOfNumber
+{
+    using System;
+
+    public class LastDigitAsWord
+    {
+        public static void Main()
+        {
+            int numberToCheck = int.Parse(Console.ReadLine());
+            Console.WriteLine(PrintLastDigitAsWord(numberToCheck));
+        }
+
+        public static string PrintLastDigitAsWord(int number)
+        {
+            string[] numbersAsWords = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+            int digit = number % 10;
+
+            return numbersAsWords[digit];
+        }
+    }
+}
---
+++ Homeworks/AdvancedC#/HomeworkMethods/Problem05ReverseNumber/ReverseNumbers.cs
@@ -0,0 +1,33 @@
+﻿namespace Problem05ReverseNumber
+{
+    using System;
+
+    public class ReverseNumbers
+    {
+        public static void Main(string[] args)
+        {
+            string numberAsString = Console.ReadLine();
+            var result = ReverseNumber(numberAsString);
+            Console.WriteLine(result);
+        }
+
+        private static double ReverseNumber(string numberAsString)
+        {
+            string reversed = string.Empty;
+
+            for (int i = numberAsString.Length - 1; i >= 0; i--)
+            {
+                reversed += numberAsString[i];
+            }
+
+            double result = double.Parse(reversed, System.Globalization.CultureInfo.InvariantCulture);
+
+            if (result % 1 == 0)
+            {
+                result = (int)result;
+            }
+
+            return result;
+        }
+    }
+}
