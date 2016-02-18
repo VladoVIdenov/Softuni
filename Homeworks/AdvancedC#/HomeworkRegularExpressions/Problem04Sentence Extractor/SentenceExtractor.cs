04a24735bab10b13ada0d1f6d617115210b92839
VladoVIdenov <videnov852@gmail.com>
2016-02-18 18:34:59 +0200

    HW Regular Expressions update

---
+++ Homeworks/AdvancedC#/HomeworkRegularExpressions/Problem04Sentence Extractor/SentenceExtractor.cs
@@ -0,0 +1,30 @@
+﻿namespace Problem04Sentence_Extractor
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    public class SentenceExtractor
+    {
+        public static void Main()
+        {
+            // No idea if this is valid solution
+            string keyword = Console.ReadLine();
+
+            string input = Console.ReadLine();
+
+            Regex regex = new Regex(@"\b[\w\s,`()'-]+[.!?]");
+
+            MatchCollection matches = regex.Matches(input);
+
+            keyword = " " + keyword + " ";
+
+            foreach (Match match in matches)
+            {
+                if (match.ToString().Contains(keyword))
+                {
+                    Console.WriteLine(match);
+                }
+            }
+        }
+    }
+}
---
+++ Homeworks/AdvancedC#/HomeworkRegularExpressions/Problem05ValidUsernames/ValidUsernames.cs
@@ -0,0 +1,40 @@
+﻿namespace Problem05ValidUsernames
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    public class ValidUsernames
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+
+            string pattern = @"\b[a-zA-Z][\w]{2,24}\b";
+
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matches = regex.Matches(input);
+
+            List<string> validUsernames = (from Match match in matches select match.ToString()).ToList();
+
+            int bestSum = 0;
+            int bestIndex = 0;
+
+            for (int i = 1; i < validUsernames.Count; i++)
+            {
+                if (validUsernames[i].Length + validUsernames[i - 1].Length > bestSum)
+                {
+                    bestSum = validUsernames[i].Length + validUsernames[i - 1].Length;
+                    bestIndex = i - 1;
+                }
+            }
+
+            Console.WriteLine(validUsernames[bestIndex]);
+            Console.WriteLine(validUsernames[bestIndex + 1]);
+
+            // judje 100 
+        }
+    }
+}
